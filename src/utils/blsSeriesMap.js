84c3b178393e69640305ece9f1f690569d85674a
RhainUnity <schmidtjeremyr@gmail.com>
2026-02-14 22:57:03 -0800

    2-14-26_login revision-2

---
+++ src/utils/blsSeriesMap.js
@@ -0,0 +1,10 @@
+src/utils/blsSeriesMap.js
+
+export const BLS_SERIES_MAP = {
+  banana: "APU0000711211",
+  milk: "APU0000709112",
+  eggs: "APU0000708111",
+  bread: "APU0000702111",
+  rice: "APU0000701312",
+  chicken: "APU0000FF1101", // example placeholder; verify the exact IDs you want
+};
