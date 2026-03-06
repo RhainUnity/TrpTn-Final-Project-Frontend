8d555ccdd6e74f9d5e1b4afcb39d3dbbe06baa3e
JeremyRhain <schmidtjeremyr@gmail.com>
2026-03-06 15:55:05 -0800

    backend validation

--- backend-express/routes/items.js
+++ backend-express/routes/items.js
@@ -3,9 +3,13 @@
 const router = require("express").Router();
 const auth = require("../middlewares/auth");
 const { getItems, createItem, deleteItem } = require("../controllers/items");
+const {
+  validateCreateItem,
+  validateItemId,
+} = require("../middlewares/validation");
 
 router.get("/items", auth, getItems);
-router.post("/items", auth, createItem);
-router.delete("/items/:itemId", auth, deleteItem);
+router.post("/items", auth, validateCreateItem, createItem);
+router.delete("/items/:itemId", auth, validateItemId, deleteItem);
 
 module.exports = router;
