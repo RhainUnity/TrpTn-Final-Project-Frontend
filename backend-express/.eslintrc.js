193814d0f0049044341ca60d00846adb2dc11c7d
JeremyRhain <schmidtjeremyr@gmail.com>
2026-03-06 22:12:12 -0800

    fixed eslint

--- backend-express/.eslintrc.js
+++ backend-express/.eslintrc.js
@@ -1,1 +1,14 @@
 // .eslintrc.js - ESLint configuration file
+module.exports = {
+  env: {
+    node: true,
+    es2021: true,
+  },
+  extends: ["airbnb-base", "prettier"],
+  parserOptions: {
+    ecmaVersion: 12,
+  },
+  rules: {
+    "no-underscore-dangle": ["error", { allow: ["_id"] }],
+  },
+};
