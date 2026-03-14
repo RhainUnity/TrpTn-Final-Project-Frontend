68c86ca97e3b64d16dadf3227cd2dd3b1b47763b
JeremyRhain <schmidtjeremyr@gmail.com>
2026-03-14 10:00:46 -0700

    preparing for deployment-connected to Mongo Atlas

--- backend-express/app.js
+++ backend-express/app.js
@@ -37,6 +37,7 @@ app.use(errorHandler);
 mongoose
   .connect(MONGO_URL)
   .then(() => {
+    console.log("Connected to MongoDB");
     app.listen(PORT, () => {
       console.log(`Server running on port ${PORT}`);
     });
--- backend-express/utils/config.js
+++ backend-express/utils/config.js
@@ -6,5 +6,5 @@ const DEFAULT_MONGO_URL = "mongodb://127.0.0.1:27017/shopping_db";
 
 module.exports = {
   JWT_SECRET: NODE_ENV === "production" ? JWT_SECRET : "dev-secret",
-  MONGO_URL: NODE_ENV === "production" ? MONGO_URL : DEFAULT_MONGO_URL,
+  MONGO_URL: MONGO_URL || DEFAULT_MONGO_URL,
 };
