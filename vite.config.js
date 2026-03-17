ab0da3d8df95b2755345d1f7bb7f66e7f7feab22
JeremyRhain <schmidtjeremyr@gmail.com>
2026-03-16 20:46:22 -0700

    working on frontend with vercel_fixed vite config

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,10 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-
-  // base:
-  //   process.env.NODE_ENV === "production"
-  //     ? "/TrpTn-Final-Project-Frontend/"
-  //     : "/",
 });
+
+/* ----- REMOVED FOR DEPLOYMENT ----- */
+// base:
+//   process.env.NODE_ENV === "production"
+//     ? "/TrpTn-Final-Project-Frontend/"
+//     : "/",
