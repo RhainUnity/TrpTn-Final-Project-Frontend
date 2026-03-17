b29312011bbd52747b6b43be49296993be01802b
JeremyRhain <schmidtjeremyr@gmail.com>
2026-03-16 20:59:56 -0700

    working on frontend with vercel_fixed authjs

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -1,6 +1,6 @@
 // src/utils/auth.js
 
-const BASE_URL = "http://localhost:3000/api";
+const BASE_URL = import.meta.env.VITE_API_URL || "http://localhost:3000/api";
 
 function checkResponse(res) {
   if (!res.ok) {
